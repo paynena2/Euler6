97c3cce4f016e9eec8762d2075e718d8fae4d44f
Allen Payne <Allen Payne@DESKTOP-88ABTDK>
2018-09-19 00:04:15 -0400

    Solved it and added comments.

--- src/Sum.java
+++ src/Sum.java
@@ -1,14 +1,24 @@
-
+//Nathaniel Payne
+//Project Euler #6
 public class Sum {
 	public static void main(String[] args) {
-		
+		int difference;
+		difference = sumPowered() - sumOfPowers(); //find the difference
+		System.out.println(difference);
 	}
 	public static int sumOfPowers() {
-		
-		return 0; //placeholder
+		int total = 0;
+		for(int x = 1; x<101; x++) {
+			total =  total + (int)(Math.pow(x, 2.0)); // square everything individually and then add to the total
+		}
+		return total;
 	}
 	public static int sumPowered() {
-		
-		return 0; //placeholder
+		int total = 0;
+		for(int x = 1; x < 101; x++) { //add everything together from 0-100
+			total = total + x;
+		}
+		total = (int) Math.pow(total, 2); // square the sum of everything
+		return total; 
 	}
 }
