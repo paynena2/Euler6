e7111996c62a3ca6d16fb80f53a6f48b6a69bb08
Allen Payne <Allen Payne@DESKTOP-88ABTDK>
2018-09-18 23:54:12 -0400

    First Commit

---
+++ src/Sum.java
@@ -0,0 +1,14 @@
+
+public class Sum {
+	public static void main(String[] args) {
+		
+	}
+	public static int sumOfPowers() {
+		
+		return 0; //placeholder
+	}
+	public static int sumPowered() {
+		
+		return 0; //placeholder
+	}
+}
